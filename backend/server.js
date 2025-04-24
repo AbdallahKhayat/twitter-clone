eeb29730e697c0ac1bc7e716571566fe2553570e
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2025-04-24 12:54:56 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("Listening to Port: " + PORT);
+  connectMongoDB();
+});
