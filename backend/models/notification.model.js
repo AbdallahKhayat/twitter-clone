97d0a03b08478d0381390b9b46d9f86efbbb4a89
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2025-04-24 19:16:04 +0300

    created notification model

---
+++ backend/models/notification.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    //if u want a specific type we use enum so send notification when follow or like
+    type: {
+      type: String,
+      required: true,
+      enum: ["follow", "like"],
+    },
+    read: {
+      type: boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+export default Notification;
