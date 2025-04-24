cc7bd56e1a25ec3e3c3538d44c57eabde04d8e54
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2025-04-24 16:40:22 +0300

    created the login functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -69,9 +69,44 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.json({
-    data: "You hit the login endpoint",
-  });
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ error: "Please provide username and password" });
+    }
+
+    const user = await User.findOne({ username: username });
+
+    // check if password is correct/valid between the one that the user entered and the one in database
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    //set the cookie
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      email: user.email,
+      followers: user.followers,
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const logout = async (req, res) => {
