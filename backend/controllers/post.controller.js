00df66100b1b61d97f0a4aafb0ac6a25147fd2b5
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2025-04-25 11:34:13 +0300

    created deletePost functionality

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -38,3 +38,31 @@ export const createPost = async (req, res) => {
     console.log("Error in createPost controller", error);
   }
 };
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    //check if the post is ours or not so we have permission to delete it
+    if (post.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "You are not authorized to delete this post" });
+    }
+
+    if (post.img) {
+      const imgId = post.img.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId);
+    }
+    // now delete post from database
+    await Post.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+    console.log("Error in deletePost controller", error);
+  }
+};
