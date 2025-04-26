38fe1a6a4bc863b2244f22e48a8bfa90bf9c9508
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2025-04-26 19:20:57 +0300

    Implemented createPost functionality while invalidating the Query to refetch posts

--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -2,23 +2,60 @@ import { CiImageOn } from "react-icons/ci";
 import { BsEmojiSmileFill } from "react-icons/bs";
 import { useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
+import { toast } from "react-hot-toast";
 const CreatePost = () => {
   const [text, setText] = useState("");
   const [img, setImg] = useState(null);
 
   const imgRef = useRef(null);
 
-  const isPending = false;
-  const isError = false;
+  const { data: authUser } = useQuery({
+    queryKey: ["authUser"],
+  });
+  const queryClient = useQueryClient();
+  const {
+    mutate: createPost,
+    isPending,
+    isError,
+    error,
+  } = useMutation({
+    mutationFn: async ({ text, img }) => {
+      try {
+        const res = await fetch("/api/posts/create", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            text,
+            img,
+          }),
+        });
 
-  const data = {
-    profileImg: "/avatars/boy1.png",
-  };
+        const data = res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Failed to create post");
+        }
+
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: async () => {
+      toast.success("Post created successfully");
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+  });
 
   const handleSubmit = (e) => {
+    setText("");
+    setImg(null);
     e.preventDefault();
-    alert("Post created successfully");
+    createPost({ text, img });
   };
 
   const handleImgChange = (e) => {
@@ -36,17 +73,17 @@ const CreatePost = () => {
     <div className="flex p-4 items-start gap-4 border-b border-gray-700">
       <div className="avatar">
         <div className="w-8 rounded-full">
-          <img src={data.profileImg || "/avatar-placeholder.png"} />
+          <img src={authUser.profileImg || "/avatar-placeholder.png"} />
         </div>
       </div>
       <form className="flex flex-col gap-2 w-full" onSubmit={handleSubmit}>
         <textarea
           className="textarea w-full p-0 text-lg resize-none border-none focus:outline-none  border-gray-800"
           placeholder="What is happening?!"
-          value={text}
+          value={createPost.text}
           onChange={(e) => setText(e.target.value)}
         />
-        {img && (
+        {createPost.img && (
           <div className="relative w-72 mx-auto">
             <IoCloseSharp
               className="absolute top-0 right-0 text-white bg-gray-800 rounded-full w-5 h-5 cursor-pointer"
@@ -56,7 +93,7 @@ const CreatePost = () => {
               }}
             />
             <img
-              src={img}
+              src={createPost.img}
               className="w-full mx-auto h-72 object-contain rounded"
             />
           </div>
@@ -81,7 +118,7 @@ const CreatePost = () => {
             {isPending ? "Posting..." : "Post"}
           </button>
         </div>
-        {isError && <div className="text-red-500">Something went wrong</div>}
+        {isError && <div className="text-red-500">{error.message}</div>}
       </form>
     </div>
   );
